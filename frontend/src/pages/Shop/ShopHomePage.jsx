3be2e8f6279e5b8428af22f62ac266c7b3e5e72c
Qurban <qa6657467@gmail.com>
2025-09-09 20:26:19 +0500

    add frontend link

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -6,11 +6,11 @@ import ShopProfileData from "../../components/shop/ShopProfileData"
 const ShopHomePage = () => {
   return (
     <div className={`${styles.section} bg-[#f5f5f5]`}>
-            <div className="w-full flex py-10  justify-between">
-              <div className="w-[25%] bg-[#fff] shadow-sm sticky left-0 z-10 rounded-[4px]  overflow-y-scroll h-[90vh] top-10 ">
+         <div className="w-full flex py-10 justify-between">
+          <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
                 <ShopInfo isOwner={true} />
               </div>
-              <div className="w-[72%] rounded-[4px]">
+          <div className="w-[72%] rounded-[4px]">
                 <ShopProfileData isOwner={true} />
               </div>
             </div>

c0ef428de574e363ad8c2c0a926f34580e46501e
Qurban <qa6657467@gmail.com>
2025-09-10 15:33:05 +0500

    set mongodb connecction

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -1,16 +1,22 @@
-
 const mongoose = require("mongoose");
 
+let isConnected;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     const connection = await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    isConnected = connection.connections[0].readyState;
     console.log(`✅ MongoDB connected: ${connection.connection.host}`);
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
-    throw new Error("authentication failed");
+    throw new Error("MongoDB connection failed");
   }
 };
 

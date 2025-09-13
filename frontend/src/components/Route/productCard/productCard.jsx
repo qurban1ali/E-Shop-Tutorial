47e5d2968ea121007cf733595787959053f06cd9
Qurban <qa6657467@gmail.com>
2025-09-13 17:30:21 +0500

    resolve error

--- frontend/src/components/Route/productCard/productCard.jsx
+++ frontend/src/components/Route/productCard/productCard.jsx
@@ -18,59 +18,59 @@ import { addToCart } from "../../../redux/actions/cart.js";
 import Ratings from "../../Products/Ratings.jsx";
 
 const ProductCard = ({ data, isEvent }) => {
-  const { cart } = useSelector((state) => state.cart);
+    const {cart} = useSelector((state) => state.cart);
   const { wishlist } = useSelector((state) => state.wishlist);
   const [click, setClick] = useState(false);
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
+  // Safe product name for URL
 
-  // check if already in wishlist
   useEffect(() => {
-    if (wishlist && data?._id && wishlist.find((i) => i._id === data._id)) {
+    if (wishlist && wishlist.find((i) => i._id === data._id)) {
       setClick(true);
     } else {
       setClick(false);
     }
-  }, [wishlist, data?._id]);
+  }, [wishlist]);
 
-  const removeFromWishlistHandler = (product) => {
-    setClick(false);
-    dispatch(removeFromWishlist(product));
+  
+
+  const removeFromWishlistHandler = (data) => {
+    setClick(!click);
+    dispatch(removeFromWishlist(data));
+  };
+  const addToWishlistHandler = (data) => {
+    setClick(!click);
+    dispatch(addToWishlist(data));
   };
 
-  const addToWishlistHandler = (product) => {
-    setClick(true);
-    dispatch(addToWishlist(product));
-  };
-
-  const addToCartHandler = (product) => {
-    const isItemExist = cart && cart.find((i) => i._id === product._id);
-    if (isItemExist) {
-      toast.error("Item already in cart");
-    } else {
-      if (product.stock < 1) {
-        toast.error("Product stock limited");
+  
+    const addToCartHandler = (id) => {
+      const isItemExist = cart && cart.find((i) => i._id === id);
+      if (isItemExist) {
+        toast.error("Item already in cart");
       } else {
-        const cartData = { ...product, qty: 1 };
+      if(data.stock < 1) {
+        toast.error("Product Stock limited")
+      }else {
+        const cartData = {...data, qty: 1};
         dispatch(addToCart(cartData));
         toast.success("Item added to cart successfully");
       }
+      }
+  
     }
-  };
-
+  
+    
   return (
     <>
       <div className="w-full h-[370px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
+        <div className="flex justify-end"></div>
+
         {/* Product Image */}
-        <Link
-          to={
-            isEvent
-              ? `/product/${data?._id}?isEvent=true`
-              : `/product/${data?._id}`
-          }
-        >
+        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
           <img
-            src={data?.images?.[0]?.url}
+            src={`${data?.images?.[0]?.url}`}
             alt={data?.name || "Product"}
             className="w-full h-[170px] object-contain"
           />
@@ -78,18 +78,14 @@ const ProductCard = ({ data, isEvent }) => {
 
         {/* Shop Name */}
         <Link to={`/shop/preview/${data?.shop?._id}`}>
-          <h5 className={styles.shop_name}>{data?.shop?.name || "Shop"}</h5>
+          <h5 className={`${styles.shop_name}`}>
+            {data?.shop?.name || "Shop"}
+          </h5>
         </Link>
 
         {/* Product Title */}
-        <Link
-          to={
-            isEvent
-              ? `/product/${data?._id}?isEvent=true`
-              : `/product/${data?._id}`
-          }
-        >
-          <h4 className="pb-3 font-[500]">
+        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
+            <h4 className="pb-3 font-[500]">
             {data?.name?.length > 40
               ? data.name.slice(0, 40) + "..."
               : data?.name || "Unnamed Product"}
@@ -97,19 +93,23 @@ const ProductCard = ({ data, isEvent }) => {
 
           {/* Rating */}
           <div className="flex">
-            <Ratings rating={data?.ratings} />
+               <Ratings rating={data?.ratings} />
           </div>
 
           {/* Price + Sales */}
           <div className="py-2 flex items-center justify-between mt-2">
             <div className="flex">
-              <h5 className={styles.productDiscountPrice}>
-                {data?.discountPrice ? `$${data.discountPrice}` : ""}
-              </h5>
+              <h5 className={`${styles.productDiscountPrice}`}>{data?.discountPrice + "$"}</h5>
+
+
               <h5 className={`${styles.price} font-[350] ml-[-7px]`}>
-                {data?.originalPrice ? `$${data.originalPrice}` : ""}
+                { data?.originalPrice}$
               </h5>
+              {/* <h4 className={`${styles.price}`}>
+                {data?.price ? data.price + "$" : null}
+              </h4> */}
             </div>
+
             <span className="font-[300] text-[17px] text-[#68d284]">
               {data?.sold_out || 0} sold
             </span>
@@ -147,7 +147,7 @@ const ProductCard = ({ data, isEvent }) => {
           <AiOutlineShoppingCart
             size={25}
             className="cursor-pointer absolute right-2 top-24"
-            onClick={() => addToCartHandler(data)}
+            onClick={() => addToCartHandler(data._id)}
             color="#444"
             title="Add to cart"
           />
